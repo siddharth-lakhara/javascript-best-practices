955375e8f625dda884658bbec60ece9c488123b9
lakhara <siddharth.lakhara@gmail.com>
2018-06-07 18:54:19 +0530

    first exercise

--- balanceManager.js
+++ balanceManager.js
@@ -1,3 +1,39 @@
+let balance = 0;
+
+const getBalance = () => balance;
+
+const canAfford = (amount) => {
+  if (!this.isValidAmount(amount)) {
+    errorMessage = 'Invalid Input';
+  }
+  if (errorMessage) {
+    throw new Error(errorMessage);
+  }
+  return amount <= balance;
+};
+
+const decreaseBalance = (amount) => {
+  // This method decreases the balance of the vending machine. If the balance amount is not
+  // enough to cover the purchase, the method throws an error.
+  let errorMessage;
+  if (!this.canAfford(amount)) {
+    errorMessage = 'Insufficient balance';
+  }
+  if (errorMessage) {
+    throw new Error(errorMessage);
+  }
+  balance -= amount;
+};
+
+const increaseBalance = (amount) => {
+  balance += amount;
+  return balance;
+};
+
 module.exports = {
-  
+  canAfford,
+  decreaseBalance,
+  balance,
+  increaseBalance,
+  getBalance,
 };
