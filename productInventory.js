e9e47ae25e9b50bea220555c30d972fb22c30276
lakhara <siddharth.lakhara@gmail.com>
2018-06-07 19:11:59 +0530

    seperation of concern 3

--- productInventory.js
+++ productInventory.js
@@ -1,3 +1,27 @@
+const products = [
+  {
+    name: 'Skittles',
+    price: 85,
+    id: 'A1',
+  },
+];
+
+const getProducts = () => products;
+
+const getProduct = (productId) => {
+  const product = products.find(p => p.id === productId);
+  return product;
+};
+
+const vendProduct = (productId) => {
+  const product = this.getProduct(productId);
+  this.decreaseBalance(product.price);
+  return product;
+};
+
 module.exports = {
-  
+  getProduct,
+  getProducts,
+  vendProduct,
+
 };
--- vendingMachine.js
+++ vendingMachine.js
@@ -3,24 +3,7 @@ const balanceManager = require('./balanceManager');
 const changeHandler = require('./changeHandler');
 const productInventory = require('./productInventory');
 
-const products = [
-  {
-    name: 'Skittles',
-    price: 85,
-    id: 'A1',
-  },
-];
-
 module.exports = {
-  getProducts() {
-    return products;
-  },
-
-  getProduct(productId) {
-    const product = products.find(p => p.id === productId);
-    return product;
-  },
-
   insertCoin(coinType) {
     const value = this.getAmount(coinType);
     this.increaseBalance(value);
@@ -30,12 +13,6 @@ module.exports = {
     const currentBalance = this.getBalance();
     this.decreaseBalance(currentBalance);
     return this.convertToChange(currentBalance);
-  },
-
-  vendProduct(productId) {
-    const product = this.getProduct(productId);
-    this.decreaseBalance(product.price);
-    return product;
   },
 
 };
